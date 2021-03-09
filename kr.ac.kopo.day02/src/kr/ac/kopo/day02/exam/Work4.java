2d779cd79a90be08ff848917c04a431e347ef03a
tmd7619 <tmd814@naver.com>
2021-03-09 17:47:04 +0900

    2021-03-09

---
+++ kr.ac.kopo.day02/src/kr/ac/kopo/day02/exam/Work4.java
@@ -0,0 +1,22 @@
+package kr.ac.kopo.day02.exam;
+
+import java.util.Scanner;
+
+public class Work4 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("첫번째 정수를 입력 :");
+		int num = sc.nextInt();
+		System.out.print("두번째 정수를 입력 :");
+		int num2 = sc.nextInt();
+		
+		if (num % num2 == 0) {
+			System.out.printf("%d가 %d의 배수인지 판단결과 : ",num, num2);
+		}
+		
+		
+		
+	}
+}
