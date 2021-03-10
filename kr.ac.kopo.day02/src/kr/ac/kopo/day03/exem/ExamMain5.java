d23132470fe0ad1e94adfcba7765a58a55747822
tmd7619 <tmd814@naver.com>
2021-03-10 17:23:29 +0900

    2021-03-10

--- kr.ac.kopo.day02/src/kr/ac/kopo/day03/exem/ExamMain5.java
+++ kr.ac.kopo.day02/src/kr/ac/kopo/day03/exem/ExamMain5.java
@@ -21,10 +21,11 @@ public class ExamMain5 {
 		//5
 		for(int i = 0; i <= 8 ; i++) { // i = 0 ~ 8
 			if(i<5) { // i - 0, 1, 2, 3 , 4
-				//별
+				//공백
 				for(int space = 1; space<= i ; space++ ) {	
 					System.out.print(" ");
 				}	
+				//별
 				for(int j=4 ; j>=i ; j--){
 					System.out.print("*");
 				}
