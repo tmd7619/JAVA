95f128ef0e416ced505d759644ce75ec9a8fc8ce
tmd7619 <tmd814@naver.com>
2021-03-14 20:32:45 +0900

    2021-03-14

--- kr.ac.kopo.day02/src/kr/ac/kopo/day05/exem/Arrhomework05.java
+++ kr.ac.kopo.day02/src/kr/ac/kopo/day05/exem/Arrhomework05.java
@@ -1,5 +1,7 @@
 package kr.ac.kopo.day05.exem;
 
+import java.util.Scanner;
+
 /*
  * 5. 별그리기 관련 문제를 라인수를 입력받아 출력하는 코드를 작성
 
@@ -30,6 +32,41 @@ public class Arrhomework05 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("3 ~ 20 사이의 라인수를 입력하세요 : ");
+		int line = sc.nextInt();
+
+		if (line % 2 == 0)
+			line = line - 1;
+
+		for (int i = 0; i < line; i++) { // 1번그림
+			for (int j = 0; j < line - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		int star = line / 2 ;		// 별의 개수가 변하는 지점(중간)
+		for (int i = 0; i < line; i++) {		// 2번그림
+			for (int j = 0; j < star; j++) {
+				System.out.print("*");
+			}
+			if (i < line / 2)
+				star--;
+			else
+				star++;
+			System.out.println();
+		}
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
 }
