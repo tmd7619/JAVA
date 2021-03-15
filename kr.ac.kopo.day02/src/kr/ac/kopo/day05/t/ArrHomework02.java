f300ae4f24821d001638da09c4d61c10755fc9ba
tmd7619 <tmd814@naver.com>
2021-03-15 10:11:03 +0900

    2021-03-15

--- kr.ac.kopo.day02/src/kr/ac/kopo/day05/t/ArrHomework02.java
+++ kr.ac.kopo.day02/src/kr/ac/kopo/day05/t/ArrHomework02.java
@@ -2,7 +2,7 @@ package kr.ac.kopo.day05.t;
 
 import java.util.Scanner;
 
-public class Homework02 {
+public class ArrHomework02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub\
@@ -15,16 +15,21 @@ public class Homework02 {
 		int oddNum = sc.nextInt();
 		
 		int[] nums = new int[evenNum + oddNum];
-		
+		/*
+		 * 짝수 : nums[0] ~ nums[evenNum-1] 
+		 * 홀수 : nums[evenNum] ~ nums[nums.length-1]
+		 */
 		int evenIdx = 0 , oddIdx = nums.length-1; // 맨 앞 인덱스 맨 뒤 인덱스 지정
 		for(int i = 0; i < nums.length; i++) {
 			System.out.print("num "+(i+1) + " : ");
 			int num = sc.nextInt();
 			
-			if(num % 2 ==0) {
+			if(num % 2 ==0 && evenIdx < evenNum) {
 				nums[evenIdx++] = num; // 앞에서부터 짝수 채우기
-			}else {
+			}else if(num % 2 == 1 && oddIdx >= evenNum ) {
 				nums[oddIdx--] = num; // 뒤에서부터 홀수 채우기
+			}else {			// 조건이 만족이 안될때 
+				i--;				
 			}
 		}
 		
