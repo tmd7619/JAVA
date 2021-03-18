0da78accda8e1dfea571e2f4783d989d65e6d6f3
tmd7619 <tmd814@naver.com>
2021-03-18 21:21:18 +0900

    2021-03-18

--- kr.ac.kopo.day02/src/kr/ac/kopo/day09/homework/StringUtil.java
+++ kr.ac.kopo.day02/src/kr/ac/kopo/day09/homework/StringUtil.java
@@ -1,22 +1,21 @@
 package kr.ac.kopo.day09.homework;
 
-import java.util.Scanner;
+/*
+ * 알파벳 아스키 코드 A-65  Z-90  a-97  z-122
+ *	65<= char <=90 이면 대문자, 97<= char <= 122 이면 소문자이다. 			
+ * 소문자 -> 대문자로 변환하려면, char에 32를 빼주고, 반대일 경우, 32를 더해준다.
+ */
+
 
 public class StringUtil {
-	Scanner sc = new Scanner(System.in);
-
 	private char inputChar;
 	private int inputNum;
 	private int inputNum02;
-	private String inputString;
-	
-	StringUtil(){
-		
-	}
+	private String inputString;	
 	
 	boolean isUpperChar(char inputChar) {			// 대문자면 true 반환하는 메소드
 		this.inputChar = inputChar;
-		if(this.inputChar>=65 && this.inputChar<=90) {
+		if(this.inputChar>=65 && this.inputChar<=90) { // 	65<= char <=90  //대문자
 			return true;
 		} else{
 			return false;
@@ -25,7 +24,7 @@ public class StringUtil {
 	
 	boolean isLowerChar(char inputChar) {			// 소문자면 true 반환하는 메소드
 		this.inputChar = inputChar;
-		if(this.inputChar>=97 && this.inputChar<=122) {
+		if(this.inputChar>=97 && this.inputChar<=122) { // 97<= char <= 122 //소문자
 			return true;
 		} else{
 			return false;
@@ -52,16 +51,51 @@ public class StringUtil {
 	}
 	StringBuilder reverseString(String str) {
 		this.inputString = str;
-		StringBuilder ans = new StringBuilder();
+		StringBuilder reverseStr = new StringBuilder();
 		for(int i = str.length()-1; i >= 0 ; i--) {
-			ans.append(str.charAt(i));
+			reverseStr.append(str.charAt(i));
 		}
 	
-		return ans;
+		return reverseStr;
 	}
 	
+	StringBuilder toUpperString(String str) {
+		this.inputString = str;
+		StringBuilder UpperStr = new StringBuilder();
+		for(int i = 0 ; i < str.length(); i ++) {
+			int a = str.charAt(i)-32;				// char에서 32를 뺸 값이 대문자	
+			UpperStr.append((char)a);
+		}
+		return UpperStr;
+	}
+	StringBuilder toLowerString(String str) {
+		this.inputString = str;
+		StringBuilder loweStr = new StringBuilder();
+		for(int i = 0 ; i < str.length(); i ++) {
+			int a = str.charAt(i)+32;			   // char에서 32를 더한 값이 소문자
+			loweStr.append((char)a);
+		}
+		return loweStr;
+	}
 	
-	
-	
+	public static int checkChar(String strData, char ch) {
+		int cnt = 0;
+		for(int i = 0 ; i < strData.length();i++) {
+			if(strData.charAt(i)==ch) {
+				cnt++;
+			}
+		}
+		
+		return cnt;
+	}
+	public static StringBuilder removeChar(String oriStr, char delChar) {
+		StringBuilder removeStr  = new StringBuilder();
+ 		for(int i = 0 ; i < oriStr.length();i++) {
+			if(oriStr.charAt(i)!=delChar) {
+				removeStr.append(oriStr.charAt(i));
+			}
+ 		}
+ 		return removeStr;
+	}
 	
 }
--- kr.ac.kopo.day02/src/kr/ac/kopo/day09/homework/StringUtilMain.java
+++ kr.ac.kopo.day02/src/kr/ac/kopo/day09/homework/StringUtilMain.java
@@ -4,17 +4,28 @@ public class StringUtilMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+
 		StringUtil util = new StringUtil();
 	
-		System.out.println(util.isUpperChar('A'));				// 1번
-		System.out.println(util.isLowerChar('A')); 				 // 2번
-		System.out.println(util.max(23,24));					// 3번
-		System.out.println(util.min(25,26));					// 4번
-		System.out.print(util.reverseString("가나다라마바사")); // 5번
+		// 메소드 및 문자열
+		System.out.println(util.isUpperChar('A'));					// 1번
+		System.out.println(util.isLowerChar('A')); 					// 2번
+		System.out.println(util.max(23,24));						// 3번
+		System.out.println(util.min(25,26));						// 4번
+		System.out.println(util.reverseString("가나다라마바사")); 	// 5번
+		System.out.println(util.toUpperString("abcdefg")); 			// 6번
+		System.out.println(util.toLowerString("ABCDEFG")); 			// 7번
 		
-
-
+		// 문자열
+		System.out.println(StringUtil.checkChar("Hello World Java", 'o')); // 1번
+		System.out.println(StringUtil.checkChar("Hello World Java", 'W'));
 		
+		System.out.println(StringUtil.removeChar("Hello World Java",'l')); // 2번
+		System.out.println(StringUtil.removeChar("Hello World Java",'o'));
+	
+
+			
 	}
 
 }
