0acb511cf77e8ba2c5eba70588ad3f34330e8b4e
tmd7619 <tmd814@naver.com>
2021-03-25 11:23:12 +0900

    2021-03-25

---
+++ kr.ac.kopo.day02/src/kr/ac/kopo/day14/GenericMain.java
@@ -0,0 +1,36 @@
+package kr.ac.kopo.day14;
+
+
+class A {
+	private Object obj;
+	
+	public A(Object obj) {
+		this.obj = obj;
+	}
+	
+	public void setObject(Object obj) {
+		this.obj = obj;
+	}
+	
+	public Object getObj() {
+		return this.obj;
+	}
+	
+	public void info() {
+		System.out.println("obj : "+obj);
+	}
+}
+
+
+
+
+public class GenericMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		
+	}
+
+}
