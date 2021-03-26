7d844ba58e8eaeb880268c5f043914da6d0d67cd
tmd7619 <tmd814@naver.com>
2021-03-26 15:06:12 +0900

    2021-03-26

---
+++ kr.ac.kopo.day02/src/kr/ac/kopo/util/FileClose.java
@@ -0,0 +1,42 @@
+package kr.ac.kopo.util;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileClose {
+	
+
+	public static void close(InputStream is) {
+		if (is != null) {
+			try {
+				is.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void close(OutputStream os) {
+		if (os != null) {
+			try {
+				os.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 오버로딩
+	public static void close(InputStream bis, InputStream is) {
+		FileClose.close(bis);
+		FileClose.close(is);
+	}
+
+	public static void close(OutputStream bos, OutputStream os) {
+		FileClose.close(bos);
+		FileClose.close(os);
+
+	}
+
+}
